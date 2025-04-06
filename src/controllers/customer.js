9e04e9c5339ce6a4aafff70fca17114e5acf1883
Sebastian23941 <168118983+Sebastian23941@users.noreply.github.com>
2025-04-05 22:22:26 -0600

    tarea

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -4,26 +4,6 @@ class CustomerController {
   }
 
   async getAll() {
-    const response = [];
-    const students = await this.service.getAllStudents();
-    for (const student of students) {
-        let status = "";
-        if (!student.debt && student.grade >= 70) {
-            status = "Aprobado";
-        } else if (!student.debt && student.grade < 70){
-            status = "Pendiente";
-        } else if (student.debt && student.grade >= 70){
-            status = "Reestructura";
-        } else {
-            status = "Expulsado";
-        }
-        response.push({"matricula": student.id, "nombre": student.name, "estatus": status});
-    }
-    return response;
-}
-
-
-  async getAll() {
     return this.service.getAllCustomers();
   
   }
@@ -47,6 +27,31 @@ class CustomerController {
     return { message: 'Customer deleted' };
   }
 }
+module.exports = CustomerController;
+
+class StudentController {
+  constructor(service) {
+    this.service = service;
+  }
+
+  async getAll() {
+      const response = [];
+      const students = await this.service.getAllStudents();
+      for (const student of students) {
+          let status = "";
+          if (!student.debt && student.grade >= 70) {
+              status = "Aprobado";
+          } else if (!student.debt && student.grade < 70){
+              status = "Pendiente";
+          } else if (student.debt && student.grade >= 70){
+              status = "Reestructura";
+          } else {
+              status = "Expulsado";
+          }
+          response.push({"matricula": student.id, "nombre": student.name, "estatus": status});
+      }
+      return response;
+  }
+}
 
 module.exports = StudentController;
-module.exports = CustomerController;
